bc91525d0c328ae1b77652e2ac7ef37adfbd1bbc
Nitin Rajan <nitin.rajan15@gmail.com>
2023-10-24 20:42:25 +0530

    New Changes

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,8 +1,10 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
+import { emojiCursor } from "cursor-effects";
 import AOS from 'aos';
 
 new MobileMenu();
+new emojiCursor({ emoji: ["😊"] });
 
 AOS.init({
     duration: 1200
